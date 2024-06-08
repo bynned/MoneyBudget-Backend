0f4e30e9d80268126235a630a108b0ab7adc230f
bynned <benny.cascarino@tuni.fi>
2024-06-08 23:44:10 +0300

    Connect mongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
+const Item = require('./models/Item');
+
 const PORT = process.env.PORT || 3030;
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
