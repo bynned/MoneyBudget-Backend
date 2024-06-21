7fecfbef7e66a4c799def2ef42113023ce9dae8e
bynned <benny.cascarino@tuni.fi>
2024-06-21 20:40:54 +0300

    Add date to item schema

--- models/Item.js
+++ models/Item.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
   amount: Number,
-  notes: String
+  notes: String,
+  date: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('dailytransactions', ItemSchema);
